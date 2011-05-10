9d51a730da16b17b8b0f1020b4a2f0f6d8a16b8e
Mattias Ryrlén <mattiasr@op5.com>
2011-05-10 20:10:54 +0200

    Redesign of EvMon, starting from zero again.
    Signed-off-by: Mattias Ryrlén <mattiasr@op5.com>

--- EvMon/__init__.py
+++ EvMon/__init__.py
@@ -1,2 +1,2 @@
 # encoding: utf-8
-"""Module Nagstamon"""
+"""Module EvMon"""
--- evmon.py
+++ evmon.py
@@ -1,15 +1,8 @@
+#!/opt/local/bin/python
 # encoding: utf-8
 
+#Master file to be executed so that EvMon starts correct
+
 import os
-import gtk
+import Queue
 
-from EvMon import GUI
-conf = ''
-servers = ''
-debug_queue = ''
-
-print "EvMon, the eventum monitor program"
-
-output = GUI.GUI(conf=conf, servers=servers, debug_queue=debug_queue)
-
-gtk.main()
